ca7531c5e98a8faf9e9f2f12a9bc1997408a3313
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-05-16 17:14:20 -0500

    extracting walker of each pet through state and displaying walker through event listener by looping through walkers array and extracting name attribute on corresponding walker object

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,16 +1,38 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
 
+document.addEventListener("click", (clickEvent) => {
+	const clickTarget = clickEvent.target
+
+	// get walkerId
+	const walkerId = clickTarget.dataset.walkerforeignkey
+
+	// find the walker object based on the walkerId that was located above
+	const allWalkers = getWalkers()
+	for (const walker of allWalkers) {
+		if (walker.id === parseInt(walkerId)) {
+			debugger
+			window.alert(`This pet is being walked by ${walker.name}`)
+		}
+	}
+})
+
 export const RegisteredPets = () => {
-    let petHTML = "<ul>"
+	let petHTML = "<ul>"
 
-    for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
-    }
+	// want to be able to view who is walking what pet when the pet is clicked on/viewed
+	// we know walkerId of the pet, but not the name of the walker
+	// need to iterate through walker array and find when walkerId foreign key of pet matches
+	// primary key of walker and extract the name from said object
 
-    petHTML += "</ul>"
+	for (const pet of pets) {
+		petHTML += `<li
+                    data-walkerForeignKey='${pet.walkerId}'           
+                    >${pet.name}</li>`
+	}
 
-    return petHTML
+	petHTML += "</ul>"
+
+	return petHTML
 }
-
